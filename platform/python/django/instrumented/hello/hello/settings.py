10094581429de9a5ef0658d67c58d465659ee604
Abhijeet Prasad <aprasad@sentry.io>
2021-07-26 19:42:55 -0400

    feat(django): Add Sentry SDK

--- platform/python/django/instrumented/hello/hello/settings.py
+++ platform/python/django/instrumented/hello/hello/settings.py
@@ -1,4 +1,13 @@
 import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn=os.environ["SENTRY_DSN"],
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+    send_default_pii=True,
+)
 
 DEBUG = False
 
