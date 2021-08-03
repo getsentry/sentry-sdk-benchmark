64bb88a2b8384599f5b06bb3d7877f90d8c073b4
Abhijeet Prasad <aprasad@sentry.io>
2021-08-03 17:03:11 -0400

    Add html template

--- bench.go
+++ bench.go
@@ -18,13 +18,19 @@ import (
 var startTime = time.Now().UTC()
 
 var dockerComposeTemplate *template.Template
+var plotFilesTemplate *template.Template
 
 func init() {
-	var err error
-	dockerComposeTemplate, err = template.ParseFiles("docker-compose.yml.tmpl")
-	if err != nil {
-		panic(err)
-	}
+	dockerComposeTemplate = template.Must(template.ParseFiles("docker-compose.yml.tmpl"))
+	plotFilesTemplate = template.Must(template.ParseFiles("template.html"))
+}
+
+type SummaryFile struct {
+	Title            string
+	BaselineHDR      string
+	InstrumentedHDR  string
+	BaselineJSON     string
+	InstrumentedJSON string
 }
 
 type Benchmark struct {
@@ -78,7 +84,7 @@ type RunResult struct {
 
 func doRun(id RunID, platform, label string, deployRelay bool) *RunResult {
 	containerName := filepath.Base(platform)
-	projectName := fmt.Sprintf("%s-%s-%s", containerName, label, id)
+	// projectName := fmt.Sprintf("%s-%s-%s", containerName, label, id)
 	contextPath := path.Join(platform, label)
 	resultPath := path.Join(append(
 		strings.Split(platform, string(os.PathSeparator))[1:],
@@ -102,22 +108,22 @@ func doRun(id RunID, platform, label string, deployRelay bool) *RunResult {
 		panic(err)
 	}
 
-	setUp(projectName, b.Bytes())
-	defer tearDown(projectName)
+	// setUp(projectName, b.Bytes())
+	// defer tearDown(projectName)
 
-	waitUntilExit("loadgen-" + id.String())
+	// waitUntilExit("loadgen-" + id.String())
 
 	result := &RunResult{
 		ComposeFile: b.Bytes(),
 		Path:        filepath.Join("result", filepath.Join(strings.Split(resultPath, "/")...)),
 	}
 
-	if err := os.MkdirAll(result.Path, 0777); err != nil {
-		panic(err)
-	}
-	if err := os.WriteFile(filepath.Join(result.Path, "docker-compose.yml"), result.ComposeFile, 0666); err != nil {
-		panic(err)
-	}
+	// if err := os.MkdirAll(result.Path, 0777); err != nil {
+	// 	panic(err)
+	// }
+	// if err := os.WriteFile(filepath.Join(result.Path, "docker-compose.yml"), result.ComposeFile, 0666); err != nil {
+	// 	panic(err)
+	// }
 
 	return result
 }
@@ -174,6 +180,41 @@ func waitUntilExit(containerName string) {
 	}
 }
 
+func read(path string) string {
+	file, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(file)
+}
+
+func generateSummary(path string) {
+	folderName := filepath.Dir(path)
+	// summaryPath = filepath.Join(folderPath, "summary.html")
+	betterPath := "result/python/django/20210803-190205-ujefeaq"
+	var b bytes.Buffer
+	err := plotFilesTemplate.Execute(&b, SummaryFile{
+		Title:            folderName,
+		BaselineHDR:      read(filepath.Join(betterPath, "baseline.hdr")),
+		InstrumentedHDR:  read(filepath.Join(betterPath, "instrumented.hdr")),
+		BaselineJSON:     read(filepath.Join(betterPath, "baseline.json")),
+		InstrumentedJSON: read(filepath.Join(betterPath, "instrumented.json")),
+	})
+	if err != nil {
+		panic(err)
+	}
+	if err := os.WriteFile(filepath.Join(betterPath, "summary.html"), b.Bytes(), 0666); err != nil {
+		panic(err)
+	}
+	// baseline.hdr
+	// instrumented.json
+	// instrumented.hdr
+	// instrumented.hdr
+}
+
 func compare(before, after *RunResult) {
 	// TODO
+	fmt.Println(before.Path)
+	generateSummary(before.Path)
 }
