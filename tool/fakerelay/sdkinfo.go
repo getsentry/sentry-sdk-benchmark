0896fe3a9ae8a0beae3dad0dd08b34210191238c
Rodolfo Carvalho <rodolfo.carvalho@sentry.io>
2021-08-23 14:55:58 +0200

    Include SDK info in Relay exposed metrics (#10)

---
+++ tool/fakerelay/sdkinfo.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bytes"
+	"regexp"
+)
+
+var t = regexp.MustCompile(`sentry_client=(\S+)`)
+
+type SDKInfo struct {
+	Name    string `json:"name"`
+	Version string `json:"version"`
+}
+
+func ParseSDKInfo(request []byte) SDKInfo {
+	var sdkInfo SDKInfo
+	match := t.FindSubmatch(request)
+	if len(match) < 2 {
+		return sdkInfo
+	}
+	s := bytes.SplitN(match[1], []byte("/"), 2)
+	sdkInfo.Name = string(bytes.ReplaceAll(s[0], []byte("-"), []byte(".")))
+	if len(s) > 1 {
+		sdkInfo.Version = string(s[1])
+	}
+	return sdkInfo
+}
