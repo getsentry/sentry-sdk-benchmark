8fb8f063df4cf541133a3df54080336fb56393a3
Rodolfo Carvalho <rodolfo.carvalho@sentry.io>
2021-09-09 23:15:15 +0200

    compare: Rewrite using filepath.WalkDir and []byte
    - We can write less code to traverse directories using filepath.WalkDir,
      instead of manually reading directories with ioutil.ReadDir.
    - New Go code should avoid using the deprecated io/ioutil package.
    - Flip if conditions to reduce indentation levels, keep code flow close
      to the left margin.
    - benchstat gives us the AddConfig API that takes []byte, so we don't
      need to write more code to wrap bytes with io.Readers
      (strings.NewReader) and io.Writers (strings.Builder).

--- compare.go
+++ compare.go
@@ -1,89 +1,74 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"io"
-	"io/ioutil"
+	"io/fs"
 	"os"
 	"path/filepath"
 	"sort"
-	"strings"
 
 	"golang.org/x/perf/benchstat"
 )
 
 // Compare compares runs from the same platform
-func Compare(s []string) {
-	builders := make(map[string]*strings.Builder)
-	for _, path := range s {
-		info, err := os.Stat(path)
-		if err != nil {
-			panic(err)
-		}
+func Compare(resultPaths []string) {
+	// m maps names like "baseline" and "instrumented" to textual data in
+	// the Go Benchmark Data Format. See https://golang.org/issue/14313.
+	m := make(map[string][]byte)
 
-		if !info.Mode().IsDir() {
-			continue
-		}
-
-		files, err := ioutil.ReadDir(path)
-		if err != nil {
-			panic(err)
-		}
-
-		for _, f := range files {
-			if f.IsDir() {
-				name := f.Name()
-				p := filepath.Join(path, name)
-				if b, ok := builders[name]; ok {
-					addResult(b, p)
-				} else {
-					var sb strings.Builder
-					builders[name] = &sb
-					addResult(&sb, p)
-				}
+	// Walk each directory from resultPaths and collect benchmark data from
+	// all result.json files.
+	for _, root := range resultPaths {
+		_ = filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				panic(err)
 			}
-		}
+			if !d.IsDir() {
+				return nil
+			}
+			defer func() {
+				// ignore panics from readTestResult
+				_ = recover()
+			}()
+			tr := readTestResult(filepath.Join(path, "result.json"))
+			m[d.Name()] = append(m[d.Name()], toGoBenchFormat(tr)...)
+			return nil
+		})
 	}
 
 	// Sort names to make output deterministic.
 	var names []string
-	for name := range builders {
+	for name := range m {
 		names = append(names, name)
 	}
 	sort.Strings(names)
 	var c benchstat.Collection
 	for _, name := range names {
-		file := strings.NewReader(builders[name].String())
-		if err := c.AddFile(name, file); err != nil {
-			panic(err)
-		}
+		c.AddConfig(name, m[name])
 	}
 
 	// Print comparison.
 	benchstat.FormatText(os.Stdout, c.Tables())
 }
 
-func addResult(w io.Writer, folderPath string) {
-	tr := readTestResult(filepath.Join(folderPath, "result.json"))
-
-	// Latencies
-	writeStat(w, "LatenciesTotal", tr.Latencies.Total.Seconds(), "s")
-	writeStat(w, "LatenciesMean", tr.Latencies.Mean.Milliseconds(), "ms")
-	writeStat(w, "Latencies50th", tr.Latencies.P50.Milliseconds(), "ms")
-	writeStat(w, "Latencies90th", tr.Latencies.P90.Milliseconds(), "ms")
-	writeStat(w, "Latencies95th", tr.Latencies.P95.Milliseconds(), "ms")
-	writeStat(w, "Latencies99th", tr.Latencies.P99.Milliseconds(), "ms")
-	writeStat(w, "LatenciesMax", tr.Latencies.Max.Milliseconds(), "ms")
-	writeStat(w, "LatenciesMin", tr.Latencies.Min.Milliseconds(), "ms")
-
-	// Other Metrics
-	writeStat(w, "Duration", tr.Duration.Milliseconds(), "ms")
-	writeStat(w, "Wait", tr.Wait.Milliseconds(), "ms")
-	writeStat(w, "Requests", tr.Requests, "req")
-	writeStat(w, "Rate", tr.Rate, "req/s")
-	writeStat(w, "Throughput", tr.Throughput, "req/s")
-}
-
-func writeStat(w io.Writer, name string, value interface{}, unit string) {
-	fmt.Fprintf(w, "Benchmark%s 1 %v %s\n", name, value, unit)
+func toGoBenchFormat(tr TestResult) []byte {
+	var b bytes.Buffer
+	writeln := func(name string, value interface{}, unit string) {
+		fmt.Fprintf(&b, "Benchmark%s 1 %v %s\n", name, value, unit)
+	}
+	writeln("LatenciesTotal", tr.Latencies.Total.Seconds(), "s")
+	writeln("LatenciesMean", tr.Latencies.Mean.Milliseconds(), "ms")
+	writeln("Latencies50th", tr.Latencies.P50.Milliseconds(), "ms")
+	writeln("Latencies90th", tr.Latencies.P90.Milliseconds(), "ms")
+	writeln("Latencies95th", tr.Latencies.P95.Milliseconds(), "ms")
+	writeln("Latencies99th", tr.Latencies.P99.Milliseconds(), "ms")
+	writeln("LatenciesMax", tr.Latencies.Max.Milliseconds(), "ms")
+	writeln("LatenciesMin", tr.Latencies.Min.Milliseconds(), "ms")
+	writeln("Duration", tr.Duration.Milliseconds(), "ms")
+	writeln("Wait", tr.Wait.Milliseconds(), "ms")
+	writeln("Requests", tr.Requests, "req")
+	writeln("Rate", tr.Rate, "req/s")
+	writeln("Throughput", tr.Throughput, "req/s")
+	return b.Bytes()
 }
