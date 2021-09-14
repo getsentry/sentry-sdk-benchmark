d3229531f1cc8ce8be17d91d0e024e801dcbe679
Abhijeet Prasad <aprasad@sentry.io>
2021-09-14 18:56:06 -0400

    feat(relay): Add total bytes recieved in Relay

--- tool/fakerelay/main.go
+++ tool/fakerelay/main.go
@@ -15,11 +15,16 @@ import (
 )
 
 var (
+	// Number of requests recieved by fakerelay instance
 	requestCount = expvar.NewInt("requests")
 
-	firstRequestOnce sync.Once
+	// string representation of first request recieved by fakerelay instance
 	firstRequest     = expvar.NewString("first_request")
+	firstRequestOnce sync.Once
 	sdkInfo          SDKInfo
+
+	// Total bytes recieved by fakerelay instance
+	bytesRecieved = expvar.NewInt("bytes_recieved")
 )
 
 func init() {
@@ -42,25 +47,29 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		requestCount.Add(1)
-		firstRequestOnce.Do(func() {
-			if r.Header.Get("Content-Encoding") == "gzip" {
-				reader, err := gzip.NewReader(r.Body)
-				if err != nil {
-					fmt.Fprintln(os.Stderr, err)
-					http.Error(w, err.Error(), 500)
-					return
-				}
-				r.Body = reader
-			}
-			b, err := httputil.DumpRequest(r, true)
+
+		if r.Header.Get("Content-Encoding") == "gzip" {
+			reader, err := gzip.NewReader(r.Body)
 			if err != nil {
 				fmt.Fprintln(os.Stderr, err)
 				http.Error(w, err.Error(), 500)
 				return
 			}
-			firstRequest.Set(string(b))
-			sdkInfo = ParseSDKInfo(b)
-		})
+			r.Body = reader
+		}
+
+		b, err := httputil.DumpRequest(r, true)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			http.Error(w, err.Error(), 500)
+		} else {
+			bytesRecieved.Add(int64(len(b)))
+			firstRequestOnce.Do(func() {
+				firstRequest.Set(string(b))
+				sdkInfo = ParseSDKInfo(b)
+			})
+		}
+
 		w.Header().Add("Content-Type", "application/json")
 		time.Sleep(80*time.Millisecond - time.Since(start))
 		fmt.Fprint(w, `{"id":"9f95bedf1f4c4487b1b4fa8eb384b48e"}`)
