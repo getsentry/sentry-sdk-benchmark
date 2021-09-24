a3d89b756cf153aaffead2aa416a854010ea5248
Abhijeet Prasad <aprasad@sentry.io>
2021-09-23 21:17:20 -0400

    report: remove google charts

--- template/js/script.js
+++ template/js/script.js
@@ -1,16 +1,15 @@
 // Based on example from: https://github.com/HdrHistogram/HdrHistogram/blob/9af106907eb6618c4c3fb5ac0da773ad0fee4f13/GoogleChartsExample/plotFiles.html
 
 // Declare constants
-const DEFAULT_TICK = { v: 1000000, f: "99.9999%" };
 const TICKS = [
-  { v: 1, f: "0%" },
-  { v: 10, f: "90%" },
-  { v: 100, f: "99%" },
-  { v: 1000, f: "99.9%" },
-  { v: 10000, f: "99.99%" },
-  { v: 100000, f: "99.999%" },
-  { v: 1000000, f: "99.9999%" },
-  { v: 10000000, f: "99.99999%" },
+  { v: 1, label: "0%" },
+  { v: 10, label: "90%" },
+  { v: 100, label: "99%" },
+  { v: 1000, label: "99.9%" },
+  { v: 10000, label: "99.99%" },
+  { v: 100000, label: "99.999%" },
+  { v: 1000000, label: "99.9999%" },
+  { v: 10000000, label: "99.99999%" },
 ];
 
 const CLASSNAMES = Object.freeze({
@@ -18,18 +17,29 @@ const CLASSNAMES = Object.freeze({
   HDR: "hdr"
 });
 
-// Declare mutable globals
-let maxPercentile = DEFAULT_TICK.v;
-let chartData = null;
-let chart = null;
+document.addEventListener("DOMContentLoaded", init);
 
-// Load the Visualization API and the corechart package.
-google.load("visualization", "1.0", { packages: ["corechart"] });
+function init() {
+  drawChart();
 
-// Set a callback to run when the Google Visualization API is loaded.
-google.setOnLoadCallback(drawInitialChart);
+  createEnvDetails();
+  formatJSON();
+}
 
-function drawInitialChart() {
+function getChartData(names, histos) {
+  while (names.length < histos.length) {
+    names.push("Unknown");
+  }
+
+  let series = [];
+  for (let i = 0; i < histos.length; i++) {
+    series = appendDataSeries(histos[i], names[i], series);
+  }
+
+  return series
+}
+
+function drawChart() {
   const histos = [];
   const names = [];
   const hdrNodes = document.querySelectorAll(getClassName(CLASSNAMES.HDR));
@@ -40,101 +50,24 @@ function drawInitialChart() {
     histos.push(node.innerHTML);
   });
 
-  setChartData(names, histos);
-  drawChart();
-  createEnvDetails();
+  const [_, ...data] = getChartData(names, histos);
 
-  formatJSON();
-}
-
-function setChartData(names, histos) {
-  while (names.length < histos.length) {
-    names.push("Unknown");
-  }
-
-  let series = [];
-  for (let i = 0; i < histos.length; i++) {
-    series = appendDataSeries(histos[i], names[i], series);
-  }
-
-  chartData = google.visualization.arrayToDataTable(series);
-}
-
-function createEnvDetails() {
-  const node = document.getElementById("requestEnv");
-  const items = node.innerHTML.split("\n").filter((i) => i !== "");
-
-  const formatNode = document.getElementById("formatEnv");
-  formatNode.innerHTML = "";
-
-  items.forEach((item) => {
-    const preEle = document.createElement("pre");
-    preEle.className = CLASSNAMES.JSON_FORMAT;
-    preEle.innerHTML = item;
-    formatNode.appendChild(preEle);
-  });
-}
-
-function formatJSON() {
-  const nodes = document.querySelectorAll(getClassName(CLASSNAMES.JSON_FORMAT));
-  nodes.forEach((node) => {
-    node.innerHTML = JSON.stringify(JSON.parse(node.innerHTML), null, 4);
-  });
-}
-
-function drawChart() {
-  const options = {
-    title: "Latency by Percentile Distribution",
-    height: 480,
-    hAxis: {
-      title: "Percentile",
-      minValue: 1,
-      logScale: true,
-      ticks: TICKS,
-      viewWindowMode: "explicit",
-      viewWindow: {
-        max: maxPercentile,
-        min: 1,
+  new Dygraph(document.getElementById('percentileLatency'), data, {
+    title: 'Latency by Percentile Distribution',
+    logscale: true,
+    ylabel: 'Latency (ms)',
+    xlabel: 'Percentile',
+    legend: 'always',
+    labels: ["Percentile"].concat(names),
+    axes: {
+      x: {
+        ticker: () => TICKS,
+        logscale: true,
       },
     },
-    vAxis: { title: "Latency (ms)", minValue: 0 },
-    legend: { position: "bottom" },
-  };
-
-  // add tooltips with correct percentile text to data:
-  const columns = [0];
-  for (let i = 1; i < chartData.getNumberOfColumns(); i++) {
-    columns.push(i);
-    columns.push({
-      type: "string",
-      properties: {
-        role: "tooltip",
-      },
-      calc: ((j) => {
-        return function (dt, row) {
-          const percentile = 100.0 - 100.0 / dt.getValue(row, 0);
-          return (
-            dt.getColumnLabel(j) +
-            ": " +
-            percentile.toPrecision(7) +
-            "%'ile = " +
-            dt.getValue(row, j) +
-            " ms"
-          );
-        };
-      })(i),
-    });
-  }
-  const view = new google.visualization.DataView(chartData);
-  view.setColumns(columns);
-
-  chart = new google.visualization.LineChart(
-    document.getElementById("chart_div")
-  );
-  chart.draw(view, options);
-
-  google.visualization.events.addListener(chart, "ready", () => {
-    chart_div.innerHTML = '<img src="' + chart.getImageURI() + '">';
+    showRoller: true,
+    strokeWidth: 1.3,
+    drawAxesAtZero: true,
   });
 }
 
@@ -184,14 +117,26 @@ function appendDataSeries(histo, name, dataSeries) {
   return series;
 }
 
-// Reacts to Slider Input: Updates Chart
-function showValue(newValue) {
-  const newPercentile = Math.pow(10, newValue);
-  const percentile = 100.0 - 100.0 / newPercentile;
-  document.getElementById("percentileRange").innerHTML = percentile + "%";
-  maxPercentile = newPercentile;
-  drawChart();
-  return { typed: "" };
+function createEnvDetails() {
+  const node = document.getElementById("requestEnv");
+  const items = node.innerHTML.split("\n").filter((i) => i !== "");
+
+  const formatNode = document.getElementById("formatEnv");
+  formatNode.innerHTML = "";
+
+  items.forEach((item) => {
+    const preEle = document.createElement("pre");
+    preEle.className = CLASSNAMES.JSON_FORMAT;
+    preEle.innerHTML = item;
+    formatNode.appendChild(preEle);
+  });
+}
+
+function formatJSON() {
+  const nodes = document.querySelectorAll(getClassName(CLASSNAMES.JSON_FORMAT));
+  nodes.forEach((node) => {
+    node.innerHTML = JSON.stringify(JSON.parse(node.innerHTML), null, 4);
+  });
 }
 
 function copy(className, runName) {
