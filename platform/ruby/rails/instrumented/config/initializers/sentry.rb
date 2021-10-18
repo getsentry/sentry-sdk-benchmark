3f10aa3388ff10333d850d07d0706c0ed2a67fd1
Neel Shah <neelshah.sa@gmail.com>
2021-10-18 18:42:35 +0200

    Update platform/ruby/rails/instrumented/config/initializers/sentry.rb
    Co-authored-by: Rodolfo Carvalho <rodolfo.carvalho@sentry.io>

--- platform/ruby/rails/instrumented/config/initializers/sentry.rb
+++ platform/ruby/rails/instrumented/config/initializers/sentry.rb
@@ -1,5 +1,4 @@
 Sentry.init do |config|
-  config.rails.report_rescued_exceptions = true
   config.breadcrumbs_logger = [:active_support_logger]
   config.traces_sample_rate = 1.0
   config.send_default_pii = true
