a17e73a8207596ff4e90c290e91739682679dac7
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2021-11-02 07:31:44 +0100

    Upgrade to sentry-java 5.3.0. Use sentry-jdbc for database queries tracing.

--- platform/java/spring/instrumented/src/main/java/hello/jpa/JpaDbRepository.java
+++ platform/java/spring/instrumented/src/main/java/hello/jpa/JpaDbRepository.java
@@ -3,7 +3,6 @@ package hello.jpa;
 import hello.model.Fortune;
 import hello.model.World;
 import hello.repository.DbRepository;
-import io.sentry.spring.tracing.SentrySpan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
@@ -24,13 +23,11 @@ public class JpaDbRepository implements DbRepository {
     }
 
     @Override
-    @SentrySpan
     public World getWorld(int id) {
         return worldRepository.findById(id).orElse(null);
     }
 
     @Override
-    @SentrySpan
     public World updateWorld(World world, int randomNumber) {
         world.randomnumber = randomNumber;
         return worldRepository.save(world);
